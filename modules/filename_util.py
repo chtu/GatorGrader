85aa5296b607d9a263ca8f716ef7f7ae2303f704
chtu <tuttle.charlie@gmail.com>
2019-02-24 01:05:16 -0800

    Added method for initializing directories

--- modules/filename_util.py
+++ modules/filename_util.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+from modules.settings import Settings as settings, Path as path
 
 
 
@@ -11,3 +12,9 @@ def insert_escape_char(filename):
 			return_str += "\\"
 		return_str += ch
 	return return_str
+
+
+def init_storage_dirs():
+	for storage_dir in path.storage_directories:
+		if not os.path.exists(storage_dir):
+			os.system(f"mkdir {storage_dir}")
