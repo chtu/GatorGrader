78404bbbcdc7f8af6a0f0b5ac77aaea0cdf8b765
Charlie T <tuttle.charlie@gmail.com>
2019-09-01 14:42:36 -0700

    Removed insert escape char

--- modules/filename_util.py
+++ modules/filename_util.py
@@ -4,17 +4,9 @@ from modules.settings import Settings as settings, Path as path
 
 
 
-def insert_escape_char(filename):
-	return_str = ""
-	for i in range(0, len(filename)):
-		ch = filename[i]
-		if ch == ' ' or ch == '(' or ch == ')':
-			return_str += "\\"
-		return_str += ch
-	return return_str
-
 
 def init_storage_dirs():
 	for storage_dir in path.storage_directories:
 		if not os.path.exists(storage_dir):
-			os.system(f"mkdir {storage_dir}")
+			# os.system(f"mkdir {storage_dir}")
+			os.mkdir(storage_dir)
