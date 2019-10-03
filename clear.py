11470c2630650723f25e7c027c9b013a761bc01a
Charlie T <tuttle.charlie@gmail.com>
2019-10-03 16:29:06 -0700

    Fixed clear

--- clear.py
+++ clear.py
@@ -10,21 +10,35 @@ def clear_folders_from_dir(dir):
 		if folder_name[0] == '.':
 			continue
 		path = os.path.join(dir, folder_name)
-		os.remove(path)
+		shutil.rmtree(path)
 
 def clear_files_from_dir(dir):
 	for filename in os.listdir(dir):
 		if filename[0] == '.':
 			continue
-		path = os.path.join(dir, fu.insert_escape_char(filename))
+		path = os.path.join(dir, filename)
+		# os.system(f"rm {path}")
 		os.remove(path)
+
+def copy_to_initial_dir(dir):
+	dir_files = os.listdir(dir)
+	for i in range(0, len(dir_files)):
+		folder_name = dir_files[i]
+		if folder_name[0] == '.':
+			continue
+		# Path to the test file
+		src_path = os.path.join(dir, (folder_name))
+		# Set destination of the file
+		dest_path = os.path.join(path.initial_sub_dir, folder_name)
+
+		shutil.copytree(src_path, dest_path)
 
 def clear_initial_dir():
 	for folder in os.listdir(path.initial_sub_dir):
 		if folder[0] == ".":
 			continue
 		if folder != settings.placeholder:
-			os.system(f"rm -rf {os.path.join(path.initial_sub_dir, folder)}")
+			shutil.rmtree(os.path.join(path.initial_sub_dir, folder))
 
 def clear_directories():
 	clear_initial_dir()
@@ -41,9 +55,15 @@ def clear_directories():
 def clear_plag_dir():
 	for filename in os.listdir(path.plagiarism_check_dir):
 		if filename != 'moss':
-			dir_path = os.path.join(path.plagiarism_check_dir, fu.insert_escape_char(filename))
+			dir_path = os.path.join(path.plagiarism_check_dir, filename)
+			
+
 			os.remove(dir_path)
 
+def clear_submission_dir():
+	for filename in os.listdir(path.initial_sub_dir):
+		if filename != settings.placeholder:
+			shutil.rmtree(os.path.join(path.initial_sub_dir, filename))
 
 
 fu.init_storage_dirs()
